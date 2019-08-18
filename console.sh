57d56cc1db5b53b6e432be2105a9f416a44bec6f
Zaytsev Kirill <Zaytzev.Kirill@yandex.ru>
2019-08-18 14:49:36 +0300

    chore: Fixed console.sh script - possibility to open container console in multiple terminal tabs

--- console.sh
+++ console.sh
@@ -1,8 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-export uid=$(id -u)
-export gid=$(id -g)
-export port_node=4280
-export port_karma=4276
+RUNNING_CONTAINER_ID="$(docker ps -qf "name=jmask_node")"
 
-docker-compose run --service-ports node bash
+if [[ -z "${RUNNING_CONTAINER_ID}" ]]
+then
+    export uid=$(id -u)
+    export gid=$(id -g)
+    export port_node=4280
+    export port_karma=4276
+
+    docker-compose run --rm --service-ports node bash
+else
+    echo "Connecting to container ${RUNNING_CONTAINER_ID}..."
+    docker exec -it --user hostuser ${RUNNING_CONTAINER_ID} bash
+fi
