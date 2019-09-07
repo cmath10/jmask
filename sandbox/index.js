9317874865ec8c7b0c2aaf349c6e517bcd755066
Zaytsev Kirill <Zaytzev.Kirill@yandex.ru>
2019-09-07 17:33:17 +0300

    chore: Added more testing fields to sandbox

--- sandbox/index.js
+++ sandbox/index.js
@@ -2,8 +2,33 @@ import JMask from '../src/jmask';
 
 (() => {
   const masks = [
+    {id: 'date', mask: '00/00/0000'},
+    {id: 'date-clear', mask: '00/00/0000', options: {clearIfNotMatch: true}},
+    {
+      id: 'date-fallback',
+      mask: '00r00r0000',
+      options: {
+        translations: {
+          'r': {
+            pattern: /[\/]/,
+            fallback: '/',
+          },
+        },
+      },
+    },
+    {
+      id: 'ip',
+      mask: '0ZZ.0ZZ.0ZZ.0ZZ',
+      options: {
+        translations: {
+          'Z': {pattern: /[0-9]/, optional: true},
+        },
+      },
+    },
     {id: 'money', mask: '#.##0,00', options: {reverse: true}},
+    {id: 'percent', mask: '##0,00%', options: {reverse: true}},
     {id: 'phone', mask: '+7-000-000-00-00'},
+    {id: 'time', mask: '00:00:00'},
   ].map(({id, mask, options}) => {
     const el = document.getElementById(id);
     return new JMask(el, mask, options);
