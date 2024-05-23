82971680c024b9575a3d1ed6835787a075ed9a5f
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2024-05-23 21:21:59 +0400

    chore: Type fixes in sandbox

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,9 +1,9 @@
-import JMask from '../src/JMask'
+import JMask from '@/JMask'
 
 (() => {
   const masks: JMask[] = []
 
-  const cep = document.getElementById('cep')
+  const cep = document.getElementById('cep') as HTMLElement
 
   masks.push(new JMask(cep, '00000-000'))
 
@@ -16,24 +16,24 @@ import JMask from '../src/JMask'
   cep.addEventListener('jmask:change', onEvent)
   cep.addEventListener('jmask:complete', onEvent)
 
-  masks.push(new JMask(document.getElementById('date'), '00/00/0000'))
-  masks.push(new JMask(document.getElementById('date-clear'), '00/00/0000', { clearIfNotMatch: true }))
-  masks.push(new JMask(document.getElementById('date-fallback'), '00r00r0000', {
+  masks.push(new JMask(document.getElementById('date') as HTMLElement, '00/00/0000'))
+  masks.push(new JMask(document.getElementById('date-clear') as HTMLElement, '00/00/0000', { clearIfNotMatch: true }))
+  masks.push(new JMask(document.getElementById('date-fallback') as HTMLElement, '00r00r0000', {
     descriptors: {
       r: { pattern: /\//, fallback: '/' },
     },
   }))
 
-  masks.push(new JMask(document.getElementById('ip'), '0ZZ.0ZZ.0ZZ.0ZZ', {
+  masks.push(new JMask(document.getElementById('ip') as HTMLElement, '0ZZ.0ZZ.0ZZ.0ZZ', {
     descriptors: {
       Z: { pattern: /[0-9]/, optional: true },
     },
   }))
 
-  masks.push(new JMask(document.getElementById('money'), '#.##0,00', { reverse: true }))
-  masks.push(new JMask(document.getElementById('percent'), '##0,00%', { reverse: true }))
-  masks.push(new JMask(document.getElementById('phone'), '+7-000-000-00-00'))
-  masks.push(new JMask(document.getElementById('time'), '00:00:00'))
+  masks.push(new JMask(document.getElementById('money') as HTMLElement, '#.##0,00', { reverse: true }))
+  masks.push(new JMask(document.getElementById('percent') as HTMLElement, '##0,00%', { reverse: true }))
+  masks.push(new JMask(document.getElementById('phone') as HTMLElement, '+7-000-000-00-00'))
+  masks.push(new JMask(document.getElementById('time') as HTMLElement, '00:00:00'))
 
   window.addEventListener('beforeunload', () => {
     masks.forEach(mask => mask.destroy())
